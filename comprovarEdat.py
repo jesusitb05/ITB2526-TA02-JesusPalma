14c47f874cef1215412b3adbf68ceecd61216fd8
jesus.palma.7e7 <jesus.palma.7e7@itb.cat>
2025-10-01 09:27:27 +0200

    Ejercicio de comprovar la edat

---
+++ comprovarEdat.py
@@ -0,0 +1,14 @@
+"""
+Jesus Palma
+1/10/2025
+ASIX1b
+
+"""
+edat=int(input("Quina edat tens?"))
+
+if edat>=18:
+    print("Ets msjor d'edat")
+else:
+    print("Ets menor")
+
+print("Programa Finalitzat")
